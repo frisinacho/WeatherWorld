1f951d84d1a414111b63d7f55db40c1afcc1e751
Nacho <frisinacho@gmail.com>
2015-07-15 10:35:53 +0200

    Lazy Getter in List<City>

--- app/src/main/java/com/nacho/weatherworld/model/Cities.java
+++ app/src/main/java/com/nacho/weatherworld/model/Cities.java
@@ -1,6 +1,15 @@
 package com.nacho.weatherworld.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cities implements ModelAggregate<City>{
+
+    private List<City> cities = new ArrayList<City>();
+
+    public Cities() {
+        cities = new ArrayList<City>();
+    }
 
     @Override
     public int size() {
@@ -11,4 +20,17 @@ public class Cities implements ModelAggregate<City>{
     public City get(int index) {
         return null;
     }
+
+    // Lazy getter
+    public List<City> getCities() {
+        if (this.cities == null) {
+            this.cities = new ArrayList<City>();
+        }
+        return cities;
+    }
+
+    public void setCities(List<City> cities) {
+        this.cities = cities;
+    }
+
 }
