d2631f5caa675466ff7c412d9f9cff6b8fe03440
Nacho <frisinacho@gmail.com>
2015-07-15 10:18:15 +0200

    City Constructor created

--- app/src/main/java/com/nacho/weatherworld/model/City.java
+++ app/src/main/java/com/nacho/weatherworld/model/City.java
@@ -1,4 +1,13 @@
 package com.nacho.weatherworld.model;
 
 public class City {
+    private String name;
+    private String temp;
+
+    public City(final String name, final String temp){
+
+        this.name = name;
+        this.temp = temp;
+
+    }
 }
