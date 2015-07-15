cf6e75da5cb819b65e1b6b96e063b273430fa211
Nacho <frisinacho@gmail.com>
2015-07-15 10:01:46 +0200

    ListView added

--- app/src/main/java/com/nacho/weatherworld/MainActivityFragment.java
+++ app/src/main/java/com/nacho/weatherworld/MainActivityFragment.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class MainActivityFragment extends Fragment {
+
+    private ListView mListCities;
 
     public MainActivityFragment() {
     }
@@ -17,6 +17,10 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mListCities = (ListView) view.findViewById(R.id.cityList);
+
+        return view;
     }
 }
