f2495acb6f9c63844670a4c67f3d99ec50f1b8c7
Nacho <frisinacho@gmail.com>
2015-07-15 10:21:22 +0200

    Getter and Setter

--- app/src/main/java/com/nacho/weatherworld/model/City.java
+++ app/src/main/java/com/nacho/weatherworld/model/City.java
@@ -4,10 +4,26 @@ public class City {
     private String name;
     private String temp;
 
-    public City(final String name, final String temp){
+    public City(/*@NonNull*/ final String name, final String temp){
 
         this.name = name;
         this.temp = temp;
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
     }
 }
