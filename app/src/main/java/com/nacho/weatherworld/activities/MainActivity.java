69b942fee2a128a250d8805dd5aad333e0345659
frisinacho <frisinacho@gmail.com>
2015-07-15 13:26:20 +0200

    "activities" package created

--- app/src/main/java/com/nacho/weatherworld/activities/MainActivity.java
+++ app/src/main/java/com/nacho/weatherworld/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.nacho.weatherworld;
+package com.nacho.weatherworld.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.nacho.weatherworld.R;
 
 public class MainActivity extends Activity {
 
