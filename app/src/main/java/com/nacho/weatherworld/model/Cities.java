bf73b8ab1e037ba025fa2ae4f72d71f3d33a8901
Nacho <frisinacho@gmail.com>
2015-07-15 10:23:27 +0200

    Cities Class and ModelAggregate Interface

---
+++ app/src/main/java/com/nacho/weatherworld/model/Cities.java
@@ -0,0 +1,5 @@
+package com.nacho.weatherworld.model;
+
+public class Cities {
+
+}
---
+++ app/src/main/java/com/nacho/weatherworld/model/ModelAggregate.java
@@ -0,0 +1,5 @@
+package com.nacho.weatherworld.model;
+
+public interface ModelAggregate {
+    public int size();
+}
