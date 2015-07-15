898270171f7301eaac6c66507945f2a1312333eb
frisinacho <frisinacho@gmail.com>
2015-07-15 16:16:08 +0200

    get Intent in CityWeatherActivity

--- app/src/main/java/com/nacho/weatherworld/activities/CityWeatherActivity.java
+++ app/src/main/java/com/nacho/weatherworld/activities/CityWeatherActivity.java
@@ -1,9 +1,12 @@
 package com.nacho.weatherworld.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.nacho.weatherworld.R;
+import com.nacho.weatherworld.model.City;
+import com.nacho.weatherworld.util.Constants;
 
 public class CityWeatherActivity extends Activity {
 
@@ -11,5 +14,9 @@ public class CityWeatherActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_weather);
+
+        Intent intent = getIntent();
+
+        City city = (City) intent.getParcelableExtra(Constants.INTENT_KEY_CITY);
     }
 }
