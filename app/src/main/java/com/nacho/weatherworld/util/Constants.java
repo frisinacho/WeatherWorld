da1ddbd229c695ca2d8dcf659b00957ed9ff6096
frisinacho <frisinacho@gmail.com>
2015-07-15 17:50:03 +0200

    Constant added

--- app/src/main/java/com/nacho/weatherworld/util/Constants.java
+++ app/src/main/java/com/nacho/weatherworld/util/Constants.java
@@ -3,4 +3,5 @@ package com.nacho.weatherworld.util;
 public class Constants {
     public static final String INTENT_KEY_CITY = "city";
     public static final String BASE_URL ="http://api.openweathermap.org/data/2.5/weather?q=%s,es&lang=es&units=metric";
+    public static final String WEATHER_ICON_URL = "http://openweathermap.org/img/w/%s.png";
 }
