c0d0eeb42c867b7dab2882549c37a0357526e185
frisinacho <frisinacho@gmail.com>
2015-07-15 17:30:34 +0200

    controls added to the fragment

--- app/src/main/java/com/nacho/weatherworld/fragments/CityWeatherActivityFragment.java
+++ app/src/main/java/com/nacho/weatherworld/fragments/CityWeatherActivityFragment.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.nacho.weatherworld.R;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class CityWeatherActivityFragment extends Fragment {
+
+    TextView cityName;
+    TextView temperature;
+    ImageView weatherIcon;
 
     public CityWeatherActivityFragment() {
     }
@@ -19,6 +22,13 @@ public class CityWeatherActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_city_weather, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_city_weather, container, false);
+
+        cityName = (TextView) view.findViewById(R.id.city_name);
+        temperature = (TextView) view.findViewById(R.id.temperature);
+        weatherIcon = (ImageView) view.findViewById(R.id.weather_icon);
+
+        return view;
     }
 }
