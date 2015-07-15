41957489526da35b338b51ca1cce01c2ac757c6d
Nacho <frisinacho@gmail.com>
2015-07-15 11:09:11 +0200

    Filling cities

--- app/src/main/java/com/nacho/weatherworld/model/MockWeatherAPI.java
+++ app/src/main/java/com/nacho/weatherworld/model/MockWeatherAPI.java
@@ -4,7 +4,7 @@ public class MockWeatherAPI {
     public static Cities getAllCities() {
         Cities allCities = new Cities();
 
-        for (int i =0; i<10; i++) {
+        for (int i =0; i<18; i++) {
             City city = new City("City " + i, "" + (i+4));
 
             allCities.add(city);
