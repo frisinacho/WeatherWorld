aba870e46df6bdff1330cc29b549afb32e324005
frisinacho <frisinacho@gmail.com>
2015-07-16 14:36:17 +0200

    some cities added to the MockWeather

--- app/src/main/java/com/nacho/weatherworld/model/MockWeatherAPI.java
+++ app/src/main/java/com/nacho/weatherworld/model/MockWeatherAPI.java
@@ -4,12 +4,25 @@ public class MockWeatherAPI {
     public static Cities getAllCities() {
         Cities allCities = new Cities();
 
-        for (int i =0; i<18; i++) {
-            City city = new City("Malaga", "" + (i+4));
-            // City city = new City("City " + i, "" + (i+4));
-
+        for (int i =0; i<1; i++) {
+            City city = new City("Málaga", "" + (i + 4));
+            allCities.add(city);
+            city = new City("Madrid", "" + (i + 4));
+            allCities.add(city);
+            city = new City("Barcelona", "" + (i + 4));
+            allCities.add(city);
+            city = new City("Berlín", "" + (i + 4));
+            allCities.add(city);
+            city = new City("Londres", "" + (i + 4));
             allCities.add(city);
         }
+
+        /*
+        for (int i=0; i<18; i++){
+            City city = new City("City " + i, "" + (i+4));
+            allCities.add(city);
+        }
+        */
 
         return allCities;
     }
